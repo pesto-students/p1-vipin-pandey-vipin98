3a70a1ac767b3f4b0d8bc8401b9fd25bb9835adb
vipin98 <pvipin907@gmail.com>
2022-06-26 20:51:18 +0530

    chnages in week 13 weather api

--- Week-13/Weather/server.js
+++ Week-13/Weather/server.js
@@ -3,6 +3,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const request = require("request");
+const e = require("express");
 const app = express();
 
 // Configure dotenv package
@@ -22,10 +23,17 @@ app.get("/", (req, res) => {
   res.send({ status: "API Working !!!" });
 });
 
-app.post("/", function (req, res) {
+app.get("/weather/MultipleCity", function (req, res) {
   // Get city name passed in the form
-  let city = req.body.city;
-  console.log("data", req.body);
+  let city = req.query.city;
+  const page = parseInt(req.query.page);
+  const limit = parseInt(req.query.limit);
+
+  const startIndex = (page - 1) * limit;
+  const endIndex = page * limit;
+  const results = {};
+
+  console.log("data", city, page, limit);
 
   let url = `http://api.openweathermap.org/data/2.5/group?id=${city}&units;=metric&appid=${apiKey}`;
 
@@ -36,9 +44,41 @@ app.post("/", function (req, res) {
       res.render(400, { weather: null, error: "Error, please try again" });
     } else {
       let weather = JSON.parse(body);
+      if (endIndex < weather.list.length)
+        results.next = {
+          page: page + 1,
+          limit: limit,
+        };
+      if (startIndex > 0) {
+        results.previous = {
+          page: page - 1,
+          limit: limit,
+        };
+      }
+      results.results = weather.list.slice(startIndex, endIndex);
+      res.json(results);
+    }
+  });
+});
 
-      // We shall output it in the console just to make sure that the data being displayed is what we want
-      console.log(weather);
+//  By x days
+app.get("/weather/City/days", function (req, res) {
+  let city = req.query.city;
+  let days = req.query.days;
+  console.log(city, days);
+  // cnt=${days}
+  // London;
+  let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&cnt=${days}&appid=${apiKey}`;
+
+  // Request for data using the URL
+  request(url, function (err, response, body) {
+    // On return, check the json data fetched
+    if (err) {
+      res.render(400, { weather: null, error: "Error, please try again" });
+    } else {
+      let weather = JSON.parse(body);
+      res.json(weather);
+      // console.log(weather);
     }
   });
 });
